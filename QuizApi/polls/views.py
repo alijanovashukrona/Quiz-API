1f318f98437123309cf00c9e6a5e671756ccec6d
alijanovashukrona <146185050+alijanovashukrona@users.noreply.github.com>
2024-04-26 21:41:02 +0500

    ./

--- QuizApi/polls/views.py
+++ QuizApi/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from random import randint
+from .models import Question
 def home(request):
-    return HttpResponse("hi")
+    questions = Question.objects.all()
+    return render(request, 'home.html', {'questions':questions})
