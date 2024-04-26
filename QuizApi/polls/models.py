8b74812d84223fc6642965de470b4f961feb664b
alijanovashukrona <146185050+alijanovashukrona@users.noreply.github.com>
2024-04-26 22:50:27 +0500

    ./

--- QuizApi/polls/models.py
+++ QuizApi/polls/models.py
@@ -6,3 +6,11 @@ class Question(models.Model):
     def __str__(self):
         return self.text
 
+class Choice(models.Model):
+    text = models.CharField(max_length=100)
+    count = models.IntegerField(default=0)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
