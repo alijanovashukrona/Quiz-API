b4b53d2537572ee0c2297358040e23b84f2b4c08
alijanovashukrona <146185050+alijanovashukrona@users.noreply.github.com>
2024-04-28 20:16:24 +0500

    last commit

--- QuizApi/polls/views.py
+++ QuizApi/polls/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from random import randint
-from .models import Question
+from .models import *
 def home(request):
     questions = Question.objects.all()
     return render(request, 'home.html', {'questions':questions})
 
 def question_detail(request, id):
+    if request.method == 'POST':
+        print(request.POST)
+        choice_id = request.POST.get('choice')
+        choice = Choice.objects.get(id=choice_id)
+        choice.count = choice.count+1
+        choice.save()
     question = Question.objects.get(id=id)
     return render (request, 'question_detail.html',{'question':question})
